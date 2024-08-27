647bcf17f7e07259ff3f77dd0387b2a9132b5580
Tom Sharratt <tsharratt55@gmail.com>
2024-08-27 16:52:37 -0400

    Update nvim config

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -8,6 +8,8 @@ return {
             -- Disable automatic setup, we are doing it manually
             vim.g.lsp_zero_extend_cmp = 0
             vim.g.lsp_zero_extend_lspconfig = 0
+
+            vim.filetype.add({ extension = { templ = "templ" } })
         end,
     },
     {
@@ -72,15 +74,18 @@ return {
 
             --- if you want to know more about lsp-zero and mason.nvim
             --- read this: https://github.com/VonHeikemen/lsp-zero.nvim/blob/v3.x/doc/md/guides/integrate-with-mason-nvim.md
-            lsp_zero.on_attach(function(client, bufnr)
+            lsp_zero.on_attach(function(_, bufnr)
                 -- see :help lsp-zero-keybindings
                 -- to learn the available actions
                 lsp_zero.default_keymaps({ buffer = bufnr })
 
-                --local opts = {buffer = bufnr, remap = false}
+                local opts = {buffer = bufnr, remap = false}
 
-                --vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
-                --vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
+                vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+                vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
+                vim.keymap.set("n", "<leader>va", function() vim.lsp.buf.code_action() end, opts)
+                vim.keymap.set("n", "<leader>vrf", function() vim.lsp.buf.references() end, opts)
+                vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
             end)
 
             require('mason-lspconfig').setup({
@@ -91,6 +96,12 @@ return {
                         -- (Optional) Configure lua language server for neovim
                         local lua_opts = lsp_zero.nvim_lua_ls()
                         require('lspconfig').lua_ls.setup(lua_opts)
+                    end,
+                    html = function()
+                        -- (Optional) Configure html language server for neovim
+                        local html_opts = lsp_zero.html_ls()
+                        html_opts.filetypes = { 'html', 'templ' }
+                        require('lspconfig').html.setup(html_opts)
                     end,
                 }
             })
